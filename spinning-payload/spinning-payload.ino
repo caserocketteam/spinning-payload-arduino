6acba97349c41e0451d0921cff238a6391374ea7
pfeeley <pgf8@case.edu>
2015-03-21 07:38:03 -0400

    Added code to get sensor data

--- spinning-payload/spinning-payload.ino
+++ spinning-payload/spinning-payload.ino
@@ -5,12 +5,58 @@
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_10DOF.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//Assign IDs to the sensors
+Adafruit_10DOF                dof   = Adafruit_10DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 
+void setup() {
+  initSensors();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //TODO: convert serial out to send over xbee
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_event_t bmp_event;
+  sensors_vec_t   orientation;
+  
+  accel.getEvent(&accel_event);
+  mag.getEvent(&mag_event);
+  
+  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Orientation: "));
+    Serial.print(orientation.roll);
+    Serial.print(F(" "));
+    Serial.print(orientation.pitch);
+    Serial.print(F(" "));
+    Serial.print(orientation.heading);
+    Serial.println(F(""));
+  }
+  
+  /* Calculate the altitude using the barometric pressure sensor */
+  bmp.getEvent(&bmp_event);
+  if (bmp_event.pressure){
+    /* Get ambient temperature in C */
+    float temperature;
+    bmp.getTemperature(&temperature);
+    /* Convert atmospheric pressure, SLP and temp to altitude */
+    Serial.print(F("Alt: "));
+    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
+                                        bmp_event.pressure,
+                                        temperature)); 
+    Serial.println(F(""));
+    /* Display the temperature */
+    Serial.print(F("Temp: "));
+    Serial.print(temperature);
+    Serial.println(F(""));
+  }
+  
+  delay(100);
+}
 
+void sendData(int data) {
+  // TODO: XBee code goes here
 }
