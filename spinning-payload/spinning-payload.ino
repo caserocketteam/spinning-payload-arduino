eb7c7d8b285175a33096a916e6e4f89b4e3da8f3
pfeeley <pgf8@case.edu>
2015-03-21 00:19:17 -0400

    added sensor libraries

--- spinning-payload/spinning-payload.ino
+++ spinning-payload/spinning-payload.ino
@@ -1,3 +1,10 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+
 void setup() {
   // put your setup code here, to run once:
 
